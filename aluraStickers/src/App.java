5895c756155bf03a40524e055343fb382a7bba36
Luan Sousa <107517953+Luan3zK@users.noreply.github.com>
2022-07-20 13:59:59 -0300

    Code da Aula 2

--- aluraStickers/src/App.java
+++ aluraStickers/src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -23,10 +25,16 @@ public class App {
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
         
         // Manipular os dados de title, image e rating.
+        var gerador = new StickersGen();
         for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println("Título: " + filme.get("title"));
-            System.out.println("Pôster: " + filme.get("image"));
-            System.out.println("Rating: " + filme.get("imDbRating") + "\n");
+            String urlImg = filme.get("image");
+            String title = filme.get("title");
+            InputStream inputStream = new URL(urlImg).openStream();
+
+            String novoArquivo = title + ".png";
+            gerador.gerar(inputStream, novoArquivo);
+
+            System.out.println("Título: " + title);
         }
     }
 }
