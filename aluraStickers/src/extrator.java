ad91e7b7a41a556de781c991a0efadb1dc42b5f0
Luan Sousa <107517953+Luan3zK@users.noreply.github.com>
2022-07-24 22:21:30 -0300

    Commit da aula5
    Adicionando o Heroku ao projeto

---
+++ aluraStickers/src/extrator.java
@@ -0,0 +1,5 @@
+import java.util.List;
+
+public interface extrator {
+    public List<conteudo> extraiConteudos(String json);
+}
---
+++ aluraStickers/src/imdbExtrator.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class imdbExtrator implements extrator {
+
+    public List<conteudo> extraiConteudos(String json){
+        // Extrair os dados.(PARSE)
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>> listAtributos = parser.parse(json);
+
+        List<conteudo> content = new ArrayList<>();
+
+        // popular a lista de conteudos
+        for (Map<String, String> atributos : listAtributos) {
+            String title = atributos.get("title");
+            String urlImg = atributos.get("image").replaceAll("(@+)(.*).jpg$", "$1.jpg");
+            var conteudo = new conteudo(title, urlImg);
+            content.add(conteudo);
+        }
+        
+        return content;
+    }
+}
